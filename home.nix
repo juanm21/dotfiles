0354ed1247d521028b5a020feb36be2f42d16294
JMDP <juanm017@gmail.com>
2026-08-11 11:32:12 -0400

    yazi

--- home.nix
+++ home.nix
@@ -119,6 +119,49 @@ in
 
   programs.htop.enable = true;
 
+  programs.yazi = {
+    enable = true;
+    shellWrapperName = "y";
+    settings = {
+      yazi = {
+        ratio = [
+          1
+          4
+          3
+        ];
+        sort_by = "natural";
+        sort_sensitive = true;
+        sort_reverse = false;
+        sort_dir_first = true;
+        linemode = "none";
+        show_hidden = true;
+        show_symlink = true;
+      };
+
+      preview = {
+        image_filter = "lanczos3";
+        image_quality = 90;
+        tab_size = 1;
+        max_width = 2000;
+        max_height = 2000;
+        cache_dir = "";
+        ueberzug_scale = 1;
+        ueberzug_offset = [
+          0
+          0
+          0
+          0
+        ];
+      };
+
+      tasks = {
+        micro_workers = 5;
+        macro_workers = 10;
+        bizarre_retry = 5;
+      };
+    };
+  };
+
   programs.zoxide = {
     enable = true;
     enableZshIntegration = true;
